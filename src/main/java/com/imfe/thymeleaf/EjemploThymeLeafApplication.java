4caabf4e0f7a90293d0d75c491fc5c83f93dc304
vmyaki <wonka.vm@gmail.com>
2017-10-20 14:43:15 +0200

    subida a git

---
+++ src/main/java/com/imfe/thymeleaf/EjemploThymeLeafApplication.java
@@ -0,0 +1,12 @@
+package com.imfe.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploThymeLeafApplication.class, args);
+	}
+}
---
+++ src/main/java/com/imfe/thymeleaf/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.imfe.thymeleaf.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+	
+	@Controller
+	public class HomeController{
+		
+		@RequestMapping(value="/")
+		public String welcome() {
+			return "welcome";
+		}
+	}
+
+
